e1d0e67afca6fb9eae3be2a7a4a674249b1681d4
lpilapil <pilapil.luke@gmail.com>
2021-10-18 19:48:33 -0700

    final lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+* Author: Luke Pilapil <lpilapil@ucsc.edu>
+* Created: October 17, 2021
+* License: Public Domain
+*/
+
+// Define Variables
+make = "Honda";
+model = "Element";
+color = "Silver";
+year = "2007";
+
+// Calculate
+age = 2021 - 2007;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
